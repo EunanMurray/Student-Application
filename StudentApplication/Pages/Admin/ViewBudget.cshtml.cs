50928b88e37b0fdd1224e07d763b90e7227343cc
Kian Gillespie <S00235539@atu,ie>
2025-03-10 14:59:40 +0000

    Added a page for viewing budget

---
+++ StudentApplication/Pages/Admin/ViewBudget.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentApplicationModel.Data;
+using StudentApplicationModel.Models;
+
+namespace StudentApplication.Pages.Admin
+{
+    public class ViewBudgetModel : PageModel
+    {
+        private readonly PrimaryContext _primaryContext;
+
+        public ViewBudgetModel(PrimaryContext context)
+        {
+            _primaryContext = context;
+        }
+
+        public IList<Budget> Budget { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Budget = await _primaryContext.Budgets.ToListAsync();
+        }
+    }
+}
